552d52bebcdb4412f71a1da669d1732ec347d098
Eero Sihvonen <sihvonen.eero@gmail.com>
2018-09-09 14:41:26 +0300

    fix file suffix

---
+++ sql/insert-into.sql
@@ -0,0 +1,3 @@
+INSERT INTO kuriirikeskus.user_roles (role_name) VALUES ('Admin');
+INSERT INTO kuriirikeskus.user_roles (role_name) VALUES ('Ajojärjestelijä');
+INSERT INTO kuriirikeskus.user_roles (role_name) VALUES ('Kuljettaja');
